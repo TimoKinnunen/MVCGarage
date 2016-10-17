e1f57dcde7f01ea2999c785fbaa2a0cab847afbc
TimoKinnunen <timo.kinnunen@bredband.net>
2016-10-17 12:21:45 +0200

    Add project files.

---
+++ MVCGarage/Models/Vehicle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCGarage.Models
+{
+    public class Vehicle
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Registration number")]
+        public string RegistrationNumber { get; set; }
+
+        [Display(Name = "Vehicle type")]
+        public VehicleType VehicleType { get; set; } //personbil, lastbil
+
+        public string Color { get; set; }
+
+        [Display(Name = "Checked in")]
+        public DateTime? StartParkingTime
+        {
+            get { return startParkingTime; }
+            set { startParkingTime = DateTime.Now; }
+        }
+
+        [Display(Name = "Checked out")]
+        public DateTime? EndParkingTime { get; set; }
+
+        [Display(Name = "Parking time")]
+        public DateTimeOffset? ParkingTime { get; set; }
+
+        [Display(Name = "Number of wheels")]
+
+        public int? NumberOfWheels { get; set; }
+        [Display(Name = "Brand and model")]
+        public string BrandAndModel { get; set; } //Saab 96,Volvo V70
+
+        //backing field
+        private DateTime startParkingTime;
+    }
+}
---
+++ MVCGarage/Models/VehicleType.cs
@@ -0,0 +1,15 @@
+﻿namespace MVCGarage.Models
+{
+    public class VehicleType
+    {
+        public string Type { get; set; }
+
+        //constructor
+        public VehicleType(string type)
+        {
+            Type = type;
+        }
+
+        private string type; // backing field for Type
+    }
+}
