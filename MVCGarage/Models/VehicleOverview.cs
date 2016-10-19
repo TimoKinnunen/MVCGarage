3bc883e028d1661732739a99f50180755304153f
johanfaxer <johan.faxer@gmail.com>
2016-10-19 11:14:00 +0200

    New class VehiclesOverview

---
+++ MVCGarage/Models/VehicleOverview.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCGarage.Models
+{
+    public class VehicleOverview
+    {
+       
+        public int Id { get; set; }
+
+        [Display(Name = "Registration number")]
+        public string RegNo { get; set; }
+
+        [Display(Name = "Type of vehicle")]
+        public string TypeName { get; set; }
+
+        [Display(Name = "Checked in")]
+        public DateTime? CheckInTime { get; set; }
+
+        [Display(Name = "Checked out")]
+        public DateTime? CheckOutTime { get; set; }
+
+        [Display(Name = "Is still in garage")]
+        public bool IsInGarage { get; set; }
+    }
+}
